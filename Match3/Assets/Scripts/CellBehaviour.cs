94ce1c9cc8bc090ab6d69a73adc95c3e51ea5fc0
Milad Katebi <mkatebi@aut.ac.nz>
2019-03-22 21:44:08 +1300

    f

---
+++ Match3/Assets/Scripts/CellBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellBehaviour : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = GameManager.Instance.sprites[1];
+    }
+
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseDrag()
+    {
+        
+    }
+}
---
+++ Match3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    public Sprite[][] Cells;
+    public int Rows;
+    public int Cols;
+    public GameObject CellPrefab;
+    public Sprite[] sprites;
+    public Text test;
+    public Text test2;
+
+    private void Start()
+    {
+        //Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
+
+
+        Cells = new Sprite[Rows][];
+        for (int i = 0; i < Rows; i++)
+            Cells[i] = new Sprite[Cols];
+
+        
+
+        for(int i = 0; i < Rows; i++)
+            for(int j = 0; j < Cols; j++)
+            {
+                GameObject instantiated = Instantiate(CellPrefab, transform, true);
+                instantiated.transform.localPosition = new Vector3(i * 2, -j * 2, 0);
+            }
+
+        //transform.position = new Vector3(0, 0, 0);
+        //transform.localPosition = new Vector3(0, 0, 0);
+
+    }
+
+    private void Update()
+    {
+        test.text = Camera.main.WorldToScreenPoint(transform.position).ToString();
+        test2.text = transform.position.ToString();
+    }
+}
